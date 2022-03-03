c617690ce93de4d78973ccea4dbdbeeec3e53625
Austin Holeman <austinholeman@gmail.com>
2022-03-03 10:06:42 -0800

    practice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // write your code here
+const URL = 'http://localhost:3000/ramens'
+const ramenMenu = document.getElementById('ramen-menu')
+
+fetch('URL')
+    .then(resp => resp.json())
+    .then(renderRamenPhotos)
+
+function renderRamenPhotos(json) {
+    const ramenPhotos = json.message
+    ramenPhotos.forEach(renderRamenPhoto)
+}
+
+function renderRamenPhoto(photoUrl) {
+    const photo = document.createElement('img')
+    photo.src = photoUrl
+    ramenMenu.append(photo)
+}
